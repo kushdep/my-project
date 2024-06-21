cb3597f80039e35704b2458f4eccfb85ffdb61cd
myprojectsmca <myprojectsmca@gmail.com>
2024-06-21 18:50:08 +0530

    Dashboard admin & user

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,11 @@ import PrivateRoute from "./components/Routes/private.js";
 import ForgotPassword from "./pages/Auth/ForgotPassword.js";
 import AdminRoute from "./components/Routes/AdminRoute.js";
 import AdminDashboard from "./pages/Admin/AdminDashboard.js"
+import CreateCategory from "./pages/Admin/CreateCategory.js";
+import CreateProduct from "./pages/Admin/CreateProduct.js";
+import Users from "./pages/Admin/Users.js";
+import Orders from "./pages/user/Orders.js";
+import Profile from "./pages/user/Profile.js";
 
 function App() {
   return (
@@ -19,11 +24,15 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/dashboard" element={<PrivateRoute />}>
           <Route path="user" element={<Dashboard/>} />
-        </Route> 
+          <Route path="user/orders" element={<Orders/>} />
+          <Route path="user/profile" element={<Profile/>} />
+          </Route> 
         <Route path="/dashboard" element={<AdminRoute />}>
           <Route path="admin" element={<AdminDashboard />} /> 
+          <Route path="admin/create-category" element={<CreateCategory />} /> 
+          <Route path="admin/create-product" element={<CreateProduct />} /> 
+          <Route path="admin/users" element={<Users />} /> 
         </Route>
-        {/* <Route path="/dashboard" element={<Dashboard/>}></Route> */}
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/login" element={<Login />} />
--- client/src/components/Layout/AdminMenu.js
+++ client/src/components/Layout/AdminMenu.js
@@ -6,15 +6,16 @@ const AdminMenu = () => {
     <>
       <div className="text-center">
         <div className="list-group">
-          <NavLink to="/dashboard/create-category" className="list-group-item list-group-item-action">
-            Create Category
-          </NavLink>
-          <NavLink to="/dashboard/create-product" className="list-group-item list-group-item-action">
-            Create Product
-          </NavLink>
-          <NavLink to="/dashboard/users" className="list-group-item list-group-item-action">
-            Users
-          </NavLink>
+          <h4>Admin Panel</h4>
+            <NavLink to="/dashboard/admin/create-category" className="list-group-item list-group-item-action">
+              Create Category
+            </NavLink>
+            <NavLink to="/dashboard/admin/create-product" className="list-group-item list-group-item-action">
+              Create Product
+            </NavLink>
+            <NavLink to="/dashboard/admin/users" className="list-group-item list-group-item-action">
+              Users
+            </NavLink>
         </div>
       </div>
     </>
