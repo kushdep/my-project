3590c9056077102ce5bfc52f72decf2f5bd92fca
myprojectsmca <myprojectsmca@gmail.com>
2024-06-30 17:20:52 +0530

    Create category page

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -1,20 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/Layout/Layout";
 import AdminMenu from "../../components/Layout/AdminMenu";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const CreateCategory = () => {
+  const [categories, setCategories] = useState([]);
+
+  //get all Categories
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/get-category");
+      if (data.success) {
+        setCategories(data.category);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong in getting category");
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+
   return (
-    <Layout title={'Dashboard - Create Category'}>
-        <div className='container-fluid m-3 p-3'>
-            <div className='row'>
-            <div className='col-md-3'>
-                <AdminMenu/>
+    <Layout title={"Dashboard - Create Category"}>
+      <div className="container-fluid m-3 p-3">
+        <div className="row">
+          <div className="col-md-3">
+            <AdminMenu />
+          </div>
+          <div className="col-md-9">
+            <h1>Manage Category</h1>
+            <div className="w-75">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {categories?.map((c) => (
+                    <>
+                      <tr>
+                        <td key={c._id}>{c.name}</td>
+                        <td>
+                          <button className="btn btn-primary">Edit</button>
+                        </td>
+                      </tr>
+                    </>
+                  ))}
+                </tbody>
+              </table>
             </div>
-            <div className='col-md-9'>
-                <h1>Create Category</h1>
-            </div>
-            </div>
+          </div>
         </div>
+      </div>
     </Layout>
   );
 };
